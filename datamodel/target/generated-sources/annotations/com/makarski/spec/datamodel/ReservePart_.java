baa422f95b5a3a190ed32e648fbd7ab9ea590d5b
san3d <san3d@mail.ru>
2016-05-13 00:28:35 +0300

    12.05.16

--- datamodel/target/generated-sources/annotations/com/makarski/spec/datamodel/ReservePart_.java
+++ datamodel/target/generated-sources/annotations/com/makarski/spec/datamodel/ReservePart_.java
@@ -1,12 +1,15 @@
 package com.makarski.spec.datamodel;
 
+import java.math.BigDecimal;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(ReservePart.class)
 public abstract class ReservePart_ {
 
+	public static volatile SingularAttribute<ReservePart, BigDecimal> quantity;
 
 }
 
--- datamodel/target/generated-sources/annotations/com/makarski/spec/datamodel/TransferPart_.java
+++ datamodel/target/generated-sources/annotations/com/makarski/spec/datamodel/TransferPart_.java
@@ -1,12 +1,15 @@
 package com.makarski.spec.datamodel;
 
+import java.math.BigDecimal;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(TransferPart.class)
 public abstract class TransferPart_ {
 
+	public static volatile SingularAttribute<TransferPart, BigDecimal> quantity;
 
 }
 
